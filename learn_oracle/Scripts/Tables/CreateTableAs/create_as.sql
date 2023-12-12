2bcf2d5d9e0cff5ab03184807933dbda503ba5ad
Vishal Bhandari <vishaljbhandari@gmail.com>
2023-12-13 02:22:07 +0530

    Updated

--- learn_oracle/Scripts/Tables/CreateTableAs/create_as.sql
+++ learn_oracle/Scripts/Tables/CreateTableAs/create_as.sql
@@ -4,42 +4,9 @@
 
 syntax -
 CREATE TABLE new_table  
-AS (SELECT * FROM old_table);   
-
-*/
-
-CREATE TABLE customers  
-( customer_id number(10) NOT NULL,  
-  customer_name varchar2(50) NOT NULL,  
-  city varchar2(50)  
-);  
-DESC customers;
-
--- Creating table from only schema of another table, use always failing where clause for source table sothat none of row gets selected
-CREATE TABLE new_customers
 AS (
 	SELECT * 
-	FROM customers 
-	WHERE 1 != 1
-);  
-DESC new_customers1;
-/* example clean up */
-DROP TABLE new_customers;
+	FROM old_table
+);   
 
-/*
-	Creating table from all columns and rows of source table
-	select all columns using * 
-	and dont use any where clause
 */
-CREATE TABLE new_customers
-AS (
-	SELECT * 
-	FROM customers 
-);  
-DESC new_customers;
-/* example clean up */
-DROP TABLE new_customers;
-
-
-/* example clean up */
-DROP TABLE customers;
---
+++ learn_oracle/Scripts/Tables/CreateTableAs/create_as_use_selective_rows_columns.sql
@@ -0,0 +1,41 @@
+-- Creating table from another table using its all rows unconditionally
+
+-- source table
+CREATE TABLE customers  
+( 
+	customer_id number(10) NOT NULL,  
+  	customer_name varchar2(50) NOT NULL,  
+  	city varchar2(50)  
+);  
+DESC customers;
+
+INSERT INTO customers VALUES (1, "Prakash", "Bangalore");
+INSERT INTO customers VALUES (2, "Anil", "Lucknow");
+INSERT INTO customers VALUES (3, "Vijay", "Bangalore");
+INSERT INTO customers VALUES (4, "Suresh", "Mumbai");
+INSERT INTO customers VALUES (5, "Prakash", "Bangalore");
+INSERT INTO customers VALUES (6, "Ajit", "Delhi");
+INSERT INTO customers VALUES (7, "Rama", "Chennai");
+INSERT INTO customers VALUES (8, "Krishna", "Nagpur");
+INSERT INTO customers VALUES (9, "Sunita", "Jaipur");
+
+SELECT * FROM customers;
+
+/*
+	Technique-
+	Use required columns in inner select query
+	User required where condition in inner select qeury
+*/
+-- To select selective columns use required columns in inner select query
+CREATE TABLE new_customers_only_names_from_bangalore
+AS (
+	SELECT customer_name
+	FROM customers 
+	WHERE city == "Bangalore"
+);  
+DESC new_customers_only_names_from_bangalore;
+SELECT * FROM new_customers_only_names_from_bangalore;
+
+/* example clean up */
+DROP TABLE customers;
+DROP TABLE new_customers_only_names_from_bangalore;
